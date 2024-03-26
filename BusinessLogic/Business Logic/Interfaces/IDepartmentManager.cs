cb766525a3c8d5656af557f277d025f12aa67469
anandswaroopinedi <158466570+anandswaroopinedi@users.noreply.github.com>
2024-03-26 16:25:31 +0530

    Delete BusinessLogic directory

--- BusinessLogic/Business Logic/Interfaces/IDepartmentManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using EmployeeDirectoryConsoleApp.Models;
-
-namespace EmployeeDirectoryConsoleApp.Interfaces
-{
-    public interface IDepartmentManager
-    {
-        public void AddDepartment(DepartmentModel dept);
-    }
-}
